f0bad7810de063f01d27d7aa5312c719c941ae7c
marciioluucas <marciioluucas@gmail.com>
2016-10-20 10:52:53 -0200

    Implementando a classe banco e criando classe pessoa extendendo cliente de pessoa

--- model/Banco.php
+++ model/Banco.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'ConfBD.php';
+require_once '../util/FuncoesString.php';
+
 /**
  * Created by PhpStorm.
  * User: Marcio
@@ -10,19 +12,32 @@ class Banco
 {
     private $mysqli;
 
+
     function __construct()
     {
-
-    }
-
-    function conexao() {
         $this->mysqli = new mysqli(ConfBD::$HOST, ConfBD::$USER, ConfBD::$PWD, ConfBD::$BASE);
         if ($this->mysqli->connect_errno) {
             echo "Erro na conexão com o banco de dados: " . $this->mysqli->connect_error;
+
         }
     }
 
-    function montarQuery() {
+    function executeQuery($query)
+    {
+        $query = FuncoesString::paraCaixaAlta($query);
+        $this->mysqli->query($query);
+    }
 
+    function descobreAcaoBanco($query) {
+        if(FuncoesString::verificaStringExistente($query, "INSERT")) {
+            return $this->mysqli->insert_id;
+        }
+        elseif(FuncoesString::verificaStringExistente($query, "UPDATE")){
+
+        }
+        elseif (FuncoesString::verificaStringExistente($query, "SELECT")){
+
+        }
+        return 'Comando nao tratado no metodo descobreAcaoBanco na classe Banco de dados';
     }
 }
---
+++ util/FuncoesString.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Márcio Lucas
+ * E-mail: marciioluucas@gmail.com
+ * Date: 20/10/2016
+ * Time: 10:41
+ */
+class FuncoesString
+{
+    public static final function paraCaixaAlta($string)
+    {
+        return strtoupper($string);
+    }
+
+    public static final function verificaStringExistente($string, $stringBuscada)
+    {
+        if (strpos($string, $stringBuscada) !== false) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
