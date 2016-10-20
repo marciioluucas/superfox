38f20399754eca071df3ff527328964f4cca6124
marciioluucas <marciioluucas@gmail.com>
2016-10-20 16:46:39 -0200

    Evitando SQL Injection

--- model/Banco.php
+++ model/Banco.php
@@ -1,6 +1,7 @@
 <?php
 require_once 'ConfBD.php';
 require_once '../util/FuncoesString.php';
+require_once '../util/FuncoesVariaveis.php';
 
 /**
  * Created by PhpStorm.
@@ -10,9 +11,15 @@ require_once '../util/FuncoesString.php';
  */
 class Banco
 {
+    /**
+     * @var mysqli
+     */
     private $mysqli;
 
 
+    /**
+     * Construtor da classe Banco .
+     */
     function __construct()
     {
         $this->mysqli = new mysqli(ConfBD::$HOST, ConfBD::$USER, ConfBD::$PWD, ConfBD::$BASE);
@@ -22,23 +29,51 @@ class Banco
         }
     }
 
-    function executeQuery($query)
+    /**
+     * @param $query
+     * @param $campos
+     * @return mixed|string
+     */
+    function executeQuery($query, $campos)
     {
+        //Lembrete: Se caso der erro, pode ser que a query do banco de dados MySQL esteja case-sensitive.
+        //Todas tabelas estão no padrão de nomes minusculos.
+        if ($query == null || $query == "") {
+            echo "Query não informada";
+        }
+
+        if ($campos == null || empty($campos)) {
+            echo "Campos não informados";
+        }
+
+        //Deve retornar a primeira letra do tipo do valor na posição de cada campo.
+        $tipos = "";
+        for ($i = 0; $i < count($campos); $i++) {
+            $campos[$i] = substr($campos[$i], 0);
+            $campos[$i] = strtolower($campos[$i]);
+            $tipos .= FuncoesVariaveis::retornaTipoVariavel($campos[$i]);
+        }
+
+        $stmt = $this->mysqli->prepare($query);
+        $stmt->bind_param($tipos, $campos);
         $query = FuncoesString::paraCaixaAlta($query);
         $this->mysqli->query($query);
         return $this->descobreAcaoBanco($query);
     }
 
-    function descobreAcaoBanco($query) {
-        if(FuncoesString::verificaStringExistente($query, "INSERT")) {
+    /**
+     * @param $query
+     * @return mixed|string
+     */
+    function descobreAcaoBanco($query)
+    {
+        if (FuncoesString::verificaStringExistente($query, "INSERT")) {
             return $this->mysqli->insert_id;
-        }
-        elseif(FuncoesString::verificaStringExistente($query, "UPDATE")){
+        } elseif (FuncoesString::verificaStringExistente($query, "UPDATE")) {
+
+        } elseif (FuncoesString::verificaStringExistente($query, "SELECT")) {
 
         }
-        elseif (FuncoesString::verificaStringExistente($query, "SELECT")){
-
-        }
-        return 'Comando nao tratado no metodo descobreAcaoBanco na classe Banco de dados';
+        return 'Comando não tratado no método descobreAcaoBanco na classe Banco de dados';
     }
 }
--- util/FuncoesString.php
+++ util/FuncoesString.php
@@ -22,4 +22,6 @@ class FuncoesString
             return false;
         }
     }
+
+    
 }
