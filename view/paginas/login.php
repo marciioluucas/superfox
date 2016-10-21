fe19974bc861d59b8b97f67cf9093f79565a718c
marciioluucas <marciioluucas@gmail.com>
2016-10-21 17:25:04 -0200

    template login

--- view/paginas/login.php
+++ view/paginas/login.php
@@ -1,7 +1,16 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Marcio
- * Date: 19/10/2016
- * Time: 19:11
- */
+<!-- * Created by PhpStorm.-->
+<!-- * User: Márcio Lucas-->
+<!-- * E-mail: marciioluucas@gmail.com-->
+<!-- * Date: 21/10/2016-->
+<!-- * Time: 16:46-->
+
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+</body>
+</html>
