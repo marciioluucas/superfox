3c94080370c79b141e8718918ee639bf11b07551
marciioluucas <marciioluucas@gmail.com>
2016-10-24 17:21:01 -0200

    Fazendo layout

---
+++ view/layout/layout.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Márcio Lucas
+ * E-mail: marciioluucas@gmail.com
+ * Date: 24/10/2016
+ * Time: 16:41
+ */
+?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="utf-8"/>
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <title>Título da Página (Estrutura básica de uma página com HTML 5)</title>
+</head>
+<body class="grey darken-3">
+<ul id="slide-out" class="side-nav fixed">
+    <li>
+        <div class="userView">
+            <img class="background" src="../dist/imgs/default-user-background.jpg">
+            <a href="#!user"><img class="circle" src="../dist/imgs/default-user-img-fox.jpg"></a>
+            <a href="#!name"><span class="white-text name">Márcio Lucas</span></a>
+            <a href="#!email"><span class="white-text email">marciioluucas@gmail.com</span></a>
+        </div>
+    </li>
+    <li><a href="#!" class="waves-effect"><i class="material-icons">dashboard</i>Dashboard</a></li>
+    <li><a href="#!" class="waves-effect"><i class="material-icons">shopping cart</i>Produtos</a></li>
+    <li><a href="#!" class="waves-effect"><i class="material-icons">people</i>Usuários</a></li>
+    <li><a href="#!" class="waves-effect"><i class="material-icons">credit_card</i> Financeiro</a></li>
+    <li><a href="#!" class="waves-effect"><i class="material-icons">person outline</i>Funcionários</a></li>
+    <li>
+        <div class="divider"></div>
+    </li>
+    <li><a class="waves-effect" href="#"><i class="material-icons">settings</i>Configurações</a></li>
+    <li><a class="waves-effect" href="#!"><i class="material-icons">exit_to_app</i>Sair</a></li>
+</ul>
+<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
+<?php
+include_once 'libs-layout.php';
+?>
+
+<script>
+
+    $(".button-collapse").sideNav();
+</script>
+</body>
+</html>
--- view/layout/libs-layout.php
+++ view/layout/libs-layout.php
@@ -1,5 +1,6 @@
 <?php
 echo "<link rel='stylesheet'  href='../libs/materializecss/css/materialize.min.css'/>\n";
+echo "<link href=\"http://fonts.googleapis.com/icon?family=Material+Icons\" rel=\"stylesheet\">";
 echo "<script src='../libs/jquery/jquery-3.1.1.min.js'></script>\n";
 echo "<script src='../libs/materializecss/js/materialize.min.js'></script>\n";
 echo "<script src='../libs/trianglify/trianglify.min.js'></script>\n";
