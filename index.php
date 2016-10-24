a5acf232f267b151af48e879e1e040c46a2cf5f4
marciioluucas <marciioluucas@gmail.com>
2016-10-24 16:38:42 -0200

    Fazendo telas

--- index.php
+++ index.php
@@ -13,31 +13,40 @@
 <head>
     <meta charset="utf-8"/>
     <meta content="width=device-width, initial-scale=1, maximum-scale=1" name="viewport">
+    <meta http-equiv="refresh" content=3;url="view/paginas/login.php?from=redirecionamento-index">
     <title>Carregando...</title>
 </head>
-<body class="grey darken-4 white-text">
-<section class="center-align" style="margin-top: 200px;">
-    <img src="view/dist/imgs/logo-superfox-login.png" alt="">
-</section>
-<section class="center-align" style="bottom: 50px; position: absolute; width: 100%">
-    <article>
-       <h4>Carregando arquivos necessários...</h4>
-    </article>
+<body class="grey darken-4 white-text container">
+<div class="row">
+    <section class="center-align col l12 m12 s12" style="margin-top: 200px;">
+        <img src="view/dist/imgs/logo-superfox-login.png" alt="">
+    </section>
 
-    <article>
-        <div class="preloader-wrapper big active">
-            <div class="spinner-layer spinner-orange-only">
-                <div class="circle-clipper left">
-                    <div class="circle"></div>
-                </div><div class="gap-patch">
-                    <div class="circle"></div>
-                </div><div class="circle-clipper right">
-                    <div class="circle"></div>
+</div>
+<div class="row">
+    <section class="col l8 m11 s11 center-align" style="bottom: 50px ; position: fixed;">
+        <article>
+            <h4>Carregando arquivos necessários...</h4>
+        </article>
+
+        <article>
+            <div class="preloader-wrapper big active">
+                <div class="spinner-layer spinner-orange-only">
+                    <div class="circle-clipper left">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="gap-patch">
+                        <div class="circle"></div>
+                    </div>
+                    <div class="circle-clipper right">
+                        <div class="circle"></div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </article>
-</section>
+        </article>
+    </section>
+
+</div>
 
 <link rel='stylesheet' href='view/libs/materializecss/css/materialize.min.css'/>
 <script src='view/libs/jquery/jquery-3.1.1.min.js'></script>
