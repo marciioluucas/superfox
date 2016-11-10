3da6742c0766ad511518dd8a7af10ee263869d73
marciioluucas <marciioluucas@gmail.com>
2016-11-10 10:47:24 -0200

    CSS atualizado no tema do SUPERFOX, Compilado o SCSS com o SASS

--- view/paginas/usuario/index.php
+++ view/paginas/usuario/index.php
@@ -14,8 +14,10 @@
         <div class="row">
             <div class="col s12">
                 <ul class="tabs">
-                    <li class="tab col s3"><a class="active" href="#listagem">Listagem</a></li>
-                    <li class="tab col s3"><a href="#cadastros">Cadastros</a></li>
+                    <li class="tab col offset-s1"></li>
+                    <li class="tab col s5"><a class="active" href="#listagem">Listagem</a></li>
+                    <li class="tab col s5"><a href="#cadastros">Cadastros</a></li>
+                    <li class="tab col offset-s1"></li>
                 </ul>
             </div>
             <div id="listagem" class="col s12"></div>
