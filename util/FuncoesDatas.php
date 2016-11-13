9e78e5f01176b87642f98e349937354f92dfd1c7
marciioluucas <marciioluu@gmail.com>
2016-11-13 12:23:20 -0200

    Implementando método na funcoes data para retornar a data por extenso.

--- util/FuncoesDatas.php
+++ util/FuncoesDatas.php
@@ -8,9 +8,15 @@
  */
 class FuncoesDatas
 {
-    public static final function converterDataParaBrasileira($data) {
+    public static final function converterDataParaBrasileira($data)
+    {
         return date('d-m-Y', strtotime($data));
     }
 
-
+    public static final function converterDataPorExtenso($data)
+    {
+        setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
+        date_default_timezone_set('America/Sao_Paulo');
+        return strftime('%A, %d de %B de %Y', strtotime($data));
+    }
 }
